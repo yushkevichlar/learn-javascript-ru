c9d9bb4656a39b9f24dca0582e22442a42cf7a51
yushkevichlar <yushkevichlar@gmail.com>
2021-08-10 22:54:07 +0300

    alert-prompt-confirm

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 // VARIABLES
 // let admin;
 // let name;
@@ -6,7 +7,17 @@
 // alert(admin);
 
 // TYPES
-let name = "Ilya";
-alert(`hello ${1}`);
-alert(`hello ${"name"}`);
-alert(`hello ${name}`);
+// let name = "Ilya";
+// alert(`hello ${1}`);
+// alert(`hello ${"name"}`);
+// alert(`hello ${name}`);
+
+// ALERT, PROMPT, CONFIRM
+let age = prompt("Сколько тебе лет?", 100);
+alert(`Тебе ${age} лет!`);
+
+let isBoss = confirm("Ты здесь главный?");
+alert(isBoss);
+
+let userName = prompt("Как тебя зовут?", "");
+alert(`Тебя зовут ${userName}`);
