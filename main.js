149d95301ca3ea6383a57a1a23234740a7a84622
yushkevichlar <yushkevichlar@gmail.com>
2021-08-10 22:36:06 +0300

    types

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-"use strict";
-let admin;
-let name;
-name = "Джон";
-admin = name;
-alert(admin);
+// VARIABLES
+// let admin;
+// let name;
+// name = "Джон";
+// admin = name;
+// alert(admin);
+
+// TYPES
+let name = "Ilya";
+alert(`hello ${1}`);
+alert(`hello ${"name"}`);
+alert(`hello ${name}`);
