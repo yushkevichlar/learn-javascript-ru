5f3ea59541be25c98b6784a9880b468ca3e5bab9
yushkevichlar <yushkevichlar@gmail.com>
2021-08-11 08:30:25 +0300

    type conversions

--- main.js
+++ main.js
@@ -13,11 +13,28 @@
 // alert(`hello ${name}`);
 
 // ALERT, PROMPT, CONFIRM
-let age = prompt("Сколько тебе лет?", 100);
-alert(`Тебе ${age} лет!`);
+// let age = prompt("Сколько тебе лет?", 100);
+// alert(`Тебе ${age} лет!`);
 
-let isBoss = confirm("Ты здесь главный?");
-alert(isBoss);
+// let isBoss = confirm("Ты здесь главный?");
+// alert(isBoss);
 
-let userName = prompt("Как тебя зовут?", "");
-alert(`Тебя зовут ${userName}`);
+// let userName = prompt("Как тебя зовут?", "");
+// alert(`Тебя зовут ${userName}`);
+
+// TYPE CONVERSIONS
+// 1. Строковое преобразование
+let value = true;
+value = String(value);
+alert(typeof value);
+// 2. Численное преобразование
+alert("6" / "2");
+let str = "123";
+alert(typeof str);
+let num = Number(str);
+alert(typeof num);
+// 3. Логическое преобразование
+alert(Boolean(1)); // true
+alert(Boolean(0)); // false
+alert(Boolean("Привет!")); // true
+alert(Boolean("")); // false
